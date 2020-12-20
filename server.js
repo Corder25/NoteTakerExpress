bf2b3a72ec491a709efe92387c42bc824976282c
Cam corder <corder2425@yahoo.com>
2020-12-19 19:22:00 -0600

    routes and file structure

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ var express = require("express");
 var app = express();
 
 // Sets a port 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 //Parse the JSON data
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
 //what folder the browser can see
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-//Bring routes
-require("./js/apiRoute")(app);
-require("./js/htmlRoute")(app);
-
+require('./routes/apiRoute')(app)
+require('./routes/htmlRoute')(app)
 //Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
